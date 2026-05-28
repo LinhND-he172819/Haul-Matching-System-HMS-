98015757670ede017ed2c9a7b2b39258ce26b0d1
LinhND-he172819 <linhndhe172819@fpt.edu.vn>
2026-05-28 19:25:28 +0700

    solution

---
+++ Haul-Matching-System-HMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Haul_Matching_System_HMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
