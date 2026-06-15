3764c445b9e5040f3e469c9134c09cd4569dd43e
@hotuannumber0 <hotuannumber0@gmail.com>
2026-06-15 10:49:40 +0700

    up to iter 1

--- HaulMatchingSystem/admin-web/src/App.tsx
+++ HaulMatchingSystem/admin-web/src/App.tsx
@@ -1,9 +1,11 @@
 // import DashboardPage from './pages/DashboardPage';
 import MatchingSuggestionPage from './pages/MatchingSuggestionPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
   // For demo, show MatchingSuggestionPage; in real app add router
   return (
+    //<DashboardPage />
     <MatchingSuggestionPage />
   )
 }
